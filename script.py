239ba9ccc753048b0f3c54f6a3899d0b6da8e1af
Jose Collado San Pedro <jocolsan@inf.upv.es>
2016-08-03 10:15:09 +0200

    write correctly in Localizable.strings

--- script.py
+++ script.py
@@ -1,1 +1,29 @@
-from sys import argv
+from pip._vendor.distlib.compat import raw_input
+
+def main():
+    addToLocalizable('Helo', 'Hola')
+
+
+
+
+def addToLocalizable(key, string):
+    localizable = open('Localizable.strings', 'a')
+    tu = (key, string)
+    string = '"{0}" = "{1}";\n'.format(*tu)
+    localizable.write(string)
+    localizable.close()
+
+
+if __name__ == '__main__':
+    main()
+
+# print("Opening the file %r" % filename)
+# target = open(filename, 'r+')
+# line = target.read(20)
+# print(line)
+# target.close()
+# print("Introduce the name of the file")
+# filename = raw_input("file: ")
+
+
+
